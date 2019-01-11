7ebaf8c7d7ab96b487d18373cd5fac1d62e9e729
Jérémy <jeremy.gfeller@cpnv.ch>
2019-01-11 18:08:03 +0100

    add a letter in the title of the project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <div class="flex-center position-ref full-height">
         <div class="content">
             <div class="title m-b-md">
-                Vedjserver
+                Vedjiserver
             </div>
 
             <div class="links">
