b6f148eac0a24e7318b96db38358500fc878053c
Jérémy <jeremy.gfeller@cpnv.ch>
2019-01-11 18:07:39 +0100

    Add button to update

--- resources/views/providers.blade.php
+++ resources/views/providers.blade.php
@@ -26,68 +26,126 @@
                             <td>{{$fournisseur->lastName}}</td>
                             <td>{{$fournisseur->companyName}}</td>
                             <td>{{$fournisseur->phone}}</td>
-                            <td><button name="del" value="{{$fournisseur->id}}">Supprimer</button></td>
+                            <td><button name="del" value="{{$fournisseur->id}}" class="btn btn-danger">Supprimer</button></td>
+                            <td>
+                                    <!-- Button trigger modal -->
+                                    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#updateModal" name="update" value="{{$fournisseur->id}}">
+                                        Modifier
+                                    </button>
+                            </td>
                         </tr>
                     @endforeach
                 </form>
+            </table>
 
-                <!-- Button trigger modal -->
-                <button type="button" style="margin-top:20px" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
-                    Ajouter un fournisseur
-                </button>
+            <!-- Button trigger modal -->
+            <button type="button" style="margin-top:20px" class="btn btn-primary" data-toggle="modal" data-target="#addModal">
+                Ajouter un fournisseur
+            </button>
 
-                <!-- Modal -->
-                <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                    <form action="/providers/add" method="post">
-                        @csrf
-                        <div class="modal-dialog modal-dialog-centered" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalCenterTitle">Ajouter un fournisseur</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
+            <!-- Modal -->
+            <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <form action="/providers/add" method="post">
+                    @csrf
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalCenterTitle">Ajouter un fournisseur</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="form-group">
+                                    <label for="title">Nom</label>
+                                    <input type="text" class="form-control" name="lastname">
                                 </div>
-                                <div class="modal-body">
-                                    <div class="form-group">
-                                        <label for="title">Nom</label>
-                                        <input type="text" class="form-control" name="lastname">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="title">Prénom</label>
-                                        <input type="text" class="form-control" name="firstname">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="title">Entreprise</label>
-                                        <input type="text" class="form-control" name="company">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="description">Téléphone</label>
-                                        <input type="text" class="form-control" name="phone">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="description">Adresse</label>
-                                        <input type="text" class="form-control" name="address">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="description">Type de fournisseur</label>
-                                        <select class="form-control" name="providerType">
-                                            <option value='0'>Client</option>
-                                            <option value='1'>Administrateur</option>
-                                            <option value='2'>Livraison</option>
-                                            <option value='3'>Fournisseur</option>
-                                        </select>
-                                    </div>
+                                <div class="form-group">
+                                    <label for="title">Prénom</label>
+                                    <input type="text" class="form-control" name="firstname">
                                 </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-                                    <button type="submit" class="btn btn-primary">Créer</button>
+                                <div class="form-group">
+                                    <label for="title">Entreprise</label>
+                                    <input type="text" class="form-control" name="company">
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">Téléphone</label>
+                                    <input type="text" class="form-control" name="phone">
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">Adresse</label>
+                                    <input type="text" class="form-control" name="address">
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">Type de fournisseur</label>
+                                    <select class="form-control" name="providerType">
+                                        <option value='0'>Client</option>
+                                        <option value='1'>Administrateur</option>
+                                        <option value='2'>Livraison</option>
+                                        <option value='3'>Fournisseur</option>
+                                    </select>
                                 </div>
                             </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                                <button type="submit" class="btn btn-primary">Créer</button>
+                            </div>
                         </div>
-                    </form>
-                </div>
-            </table>
+                    </div>
+                </form>
+            </div>
+
+            <!-- Modal -->
+            <div class="modal fade" id="updateModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                <form action="/providers/add" method="post">
+                    @csrf
+                    <div class="modal-dialog modal-dialog-centered" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalCenterTitle">Ajouter un fournisseur</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="form-group">
+                                    <label for="title">Nom</label>
+                                    <input type="text" class="form-control" name="lastname">
+                                </div>
+                                <div class="form-group">
+                                    <label for="title">Prénom</label>
+                                    <input type="text" class="form-control" name="firstname">
+                                </div>
+                                <div class="form-group">
+                                    <label for="title">Entreprise</label>
+                                    <input type="text" class="form-control" name="company">
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">Téléphone</label>
+                                    <input type="text" class="form-control" name="phone">
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">Adresse</label>
+                                    <input type="text" class="form-control" name="address">
+                                </div>
+                                <div class="form-group">
+                                    <label for="description">Type de fournisseur</label>
+                                    <select class="form-control" name="providerType">
+                                        <option value='0'>Client</option>
+                                        <option value='1'>Administrateur</option>
+                                        <option value='2'>Livraison</option>
+                                        <option value='3'>Fournisseur</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                                <button type="submit" class="btn btn-primary">Créer</button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
