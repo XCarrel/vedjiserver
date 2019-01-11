887bfd37b225b9f2172d912de8f21044a89774fe
Jérémy <jeremy.gfeller@cpnv.ch>
2019-01-11 14:56:59 +0100

    Import bootstrap in the layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <script src="{{asset('js/app.js')}}" defer></script>
+        <link href="{{asset('css/app.css')}}" rel="stylesheet">
+
         <title>Vedjiserver</title>
 
         <!-- Styles -->
