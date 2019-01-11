2cd705a8119cf0cd1a732c30d3b0b9ea2a8ad33d
Jérémy <jeremy.gfeller@cpnv.ch>
2019-01-11 09:59:58 +0100

    Modify the function in the controller

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,14 +4,21 @@ namespace App\Http\Controllers;
 
 use App\Products;
 use App\User;
+use App\Users;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
 class AdminController extends Controller
 {
-    public function index()
+    public function indexProducts()
     {
         $products = Products::all();
-        return view('admin')->with('products', $products);
+        return view('products')->with('products', $products);
+    }
+
+    public function indexProviders()
+    {
+        $fournisseurs = Users::all();
+        return view('providers')->with('fournisseurs', $fournisseurs);
     }
 }
