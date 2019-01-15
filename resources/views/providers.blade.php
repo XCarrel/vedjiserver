f1984b896480dec11f1f776f0e9f2126e9430da0
Jérémy <jeremy.gfeller@cpnv.ch>
2019-01-15 08:05:36 +0100

    Delete the modale for update

--- resources/views/providers.blade.php
+++ resources/views/providers.blade.php
@@ -32,8 +32,10 @@
                             </form>
                         </td>
                         <td>
-                            <!-- Button trigger modal -->
-                            <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#updateModal" name="update" value="{{$fournisseur->id}}" >Modifier</button>
+                            <form method="post" action="/providers/update">
+                            @csrf
+                                <button name="update" value="{{$fournisseur->id}}" class="btn btn-warning">Modifier</button>
+                            </form>
                         </td>
                     </tr>
                 @endforeach
@@ -59,27 +61,27 @@
                             <div class="modal-body">
                                 <div class="form-group">
                                     <label for="title">Nom</label>
-                                    <input type="text" class="form-control" name="lastname">
+                                    <input type="text" class="form-control" name="lastname" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="title">Prénom</label>
-                                    <input type="text" class="form-control" name="firstname">
+                                    <input type="text" class="form-control" name="firstname" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="title">Entreprise</label>
-                                    <input type="text" class="form-control" name="company">
+                                    <input type="text" class="form-control" name="company" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="description">Téléphone</label>
-                                    <input type="text" class="form-control" name="phone">
+                                    <input type="text" class="form-control" name="phone" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="description">Adresse</label>
-                                    <input type="text" class="form-control" name="address">
+                                    <input type="text" class="form-control" name="address" required>
                                 </div>
                                 <div class="form-group">
                                     <label for="description">Type de fournisseur</label>
-                                    <select class="form-control" name="providerType">
+                                    <select class="form-control" name="providerType" required>
                                         <option value='0'>Client</option>
                                         <option value='1'>Administrateur</option>
                                         <option value='2'>Livraison</option>
@@ -95,60 +97,6 @@
                     </div>
                 </form>
             </div>
-
-
-                <!-- Modal -->
-                <div class="modal fade" id="updateModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                    <form action="/providers/update" method="post">
-                        @csrf
-                        <div class="modal-dialog modal-dialog-centered" role="document">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <h5 class="modal-title" id="exampleModalCenterTitle">Ajouter un fournisseur</h5>
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                                <div class="modal-body">
-                                    <div class="form-group">
-                                        <label for="title">Nom</label>
-                                        <input type="text" class="form-control" name="lastname">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="title">Prénom</label>
-                                        <input type="text" class="form-control" name="firstname">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="title">Entreprise</label>
-                                        <input type="text" class="form-control" name="company">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="description">Téléphone</label>
-                                        <input type="text" class="form-control" name="phone">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="description">Adresse</label>
-                                        <input type="text" class="form-control" name="address">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="description">Type de fournisseur</label>
-                                        <select class="form-control" name="providerType">
-                                            <option value='0'>Client</option>
-                                            <option value='1'>Administrateur</option>
-                                            <option value='2'>Livraison</option>
-                                            <option value='3'>Fournisseur</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-                                    <button type="submit" class="btn btn-primary">Mettre à jour</button>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-
         </div>
     </div>
 @endsection
